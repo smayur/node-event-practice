8ec7e1dbf4638cc778514e8544f37759884f92c9
mayurS <mayurs@prdxn.com>
2021-01-04 16:57:39 +0530

    Passing arguments and this to listeners example

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ myEmitter.on('event', () => {
   console.log('an event occurred!');
 });
 myEmitter.emit('event');
+
+
+// Passing arguments and this to listeners
+
+myEmitter.on('param', function(a, b) {
+  console.log(a, b, this, this === myEmitter);
+  // Prints:
+  // a b MyEmitter {
+  //   _events: [Object: null prototype] { event: [Function], param: [Function] },
+  //   _eventsCount: 2,
+  //   _maxListeners: undefined,
+  //   [Symbol(kCapture)]: false
+  // } true
+});
+myEmitter.emit('param', 'a', 'b');
