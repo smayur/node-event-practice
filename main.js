387521c6ecfa4c6683b6c8505b80f1e7c21df558
mayurS <mayurs@prdxn.com>
2021-01-06 21:25:44 +0530

    modeule theory added

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const EventEmitter = require('events');
 const net = require("net");
 const fs = require("fs");
-const circle = require('./circle.js');
+const circle = require('./circle');
 
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
@@ -99,3 +99,50 @@ console.log("Will read the file 2nd time!!...");
 
 /*********** Modules ***********/ 
 console.log(`The area of a circle of radius 4 is ${circle.area(4)}`);
+
+/* 
+In the Node.js module system, each file is treated as a separate module.
+
+When a file is run directly from Node.js, require.main is set to its module. 
+That means that it is possible to determine whether a file has been run directly 
+by testing require.main === module. 
+
+For a file main.js, this will be true if run via node mian.js, but false if run by require('./main').
+
+**-- The .mjs extension
+  It is not possible to require() files that have the .mjs extension.
+  Attempting to do so will throw an error. (Must use import to load ES Module)
+
+  Careful planning is required to allow cyclic module dependencies to work correctly within an application.
+
+**-- File modules
+
+  If the exact filename is not found, then Node.js will attempt to load the required filename 
+  with the added extensions: .js, .json, and finally .node.
+
+  A required module prefixed with '/' is an absolute path to the file. 
+  For example, require('/home/marco/foo.js') will load the file at /home/marco/foo.js.
+
+  A required module prefixed with './' is relative to the file calling require(). 
+  That is, circle.js must be in the same directory as foo.js for require('./circle') to find it.
+
+  Without a leading '/', './', or '../' to indicate a file, 
+  the module must either be a core module or is loaded from a node_modules folder.
+
+  If the given path does not exist, require() will throw an Error with its code property set to 'MODULE_NOT_FOUND'.
+
+**--The module wrapper
+  Before a module's code is executed, Node.js will wrap it with a function wrapper that looks like the following:
+
+  (function(exports, require, module, __filename, __dirname) {
+    // Module code actually lives in here
+  });
+
+  By doing this, Node.js achieves a few things:
+
+  - It keeps top-level variables (defined with var, const or let) scoped to the module rather than the global object.
+  - It helps to provide some global-looking variables that are actually specific to the module, such as:
+      - The module and exports objects that the implementor can use to export values from the module.
+      - The convenience variables __filename and __dirname, containing the module's absolute filename and directory path.
+*/
+
